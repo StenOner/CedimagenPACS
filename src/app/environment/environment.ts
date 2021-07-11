c19b39d30cdd3b2e4181a00492e633e6f9b6257c
Stenoi <stenoner90@gmail.com>
2021-07-11 17:11:25 -0500

    environment

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,10 +1,10 @@
 export const Environment={
     url:'http://192.168.0.100:3700/',
-    accessKey:'CEDIMAGEN_PACS_accessToken',
-    refreshKey:'CEDIMAGEN_PACS_refreshToken',
-    rememberMeKey:'CEDIMAGEN_PACS-rememberMe',
-    rememberEmailKey:'CEDIMAGEN_PACS-rememberEmail',
-    recoveryPasswordKey:'CEDIMAGEN_PACS-recoveryPasswordToken',
+    accessKey:'CEDIMAGEN_PACS__accessToken',
+    refreshKey:'CEDIMAGEN_PACS__refreshToken',
+    rememberMeKey:'CEDIMAGEN_PACS__rememberMe',
+    rememberEmailKey:'CEDIMAGEN_PACS__rememberEmail',
+    recoveryPasswordKey:'CEDIMAGEN_PACS__recoveryPasswordToken',
     admin:'5f76b4f32c085422e8fe52e6',
     doctor:'5f76b5102c085422e8fe52e7',
     client:'5f76b5172c085422e8fe52e8'
